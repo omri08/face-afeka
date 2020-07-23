58ac0bc650d72c2cea8f85e474e9699bd5744131
omri08 <54754198+omri08@users.noreply.github.com>
2020-07-23 14:46:10 +0300

    Added user model and created user route

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const colors = require("colors");
+const helmet = require("helmet");
+
+const connectDB = require("./config/db");
+
+const app = express();
+
+// Load env vars
+dotenv.config({ path: "./config/config.env" });
+
+// Init Middleware
+app.use(express.json());
+
+// Connect to database
+connectDB();
+
+//Define Routes
+app.use("/api/users", require("./routes/user"));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
